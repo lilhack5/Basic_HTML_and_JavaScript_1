32b4bf1068ecefbca43a434f490704e1f2bd0a23
lilhack5 <114317539+lilhack5@users.noreply.github.com>
2022-10-02 11:15:34 -0700

    JavaScript
    Made new repository and added files

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Finally learning JavaScript after 20 years!!!!!")
+function My_First_Function()	{
+		var String = "Kissme, I'm Irish!";
+		var result = Sting.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
