4ad9462118d456df74bcbfb9f879c36e0183f75d
lilhack5 <114317539+lilhack5@users.noreply.github.com>
2022-10-30 15:33:24 -0700

    made new html and jv files
    Made new html and JavaScript to go with it

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+var B = ("Hello, World");
+alert(B); // This creates an alert box that you see before entering the page.
+
+var A = ("Shaun yelled, \"Huntyer it's your turn to talk to dad!\" \"I'm right here!\" responded Huntyer.") // This allows me to add quatation marks.
+document.write(A); // This tells the page to show the string for var a
+
+var Family = "The Smiths", Dad = "Bryan", Mom = "Tiffany", Daughter = "Huntyer", Son = "Shaun"; // Here I gave multiple meanings to words
+document.write(Son);// Here I picked one of the variables from family and showed just that on the page.
+
+document.write(5 * 5);//Here is an expression which will show the result of 25 on the page.
+
+var Sent1 = "This is the beginning of the string"; //Creating part 1 of 2 strings to add together.
+
+var Sent2 = "and this is the end of the string";//Creating part 2 of 2 strings to add together.
+
+document.write(Sent1 + Sent2);//This will take both variables and form a sentance.
