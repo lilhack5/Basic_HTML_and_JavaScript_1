2189ad02ee6193bb98f483f34b02968cc5951083
lilhack5 <114317539+lilhack5@users.noreply.github.com>
2022-10-30 16:12:05 -0700

    made new folder
    new folder and function js and html

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function displayDate() {//This will show the date and time once clicked
+    document.getElementById("demo").innerHTML = Date();
+}
+
+function myFunction() { //This creates a new function
+    var sentence = "I am learning";//Made a variable sentence
+    sentence += " a lot from this course.";//Adds variable sentence to new text
+    document.getElementById("Concatenate").innerHTML = sentence;//What is used to show on html
+}
